bb21f5044a34848e270e3a2cef2ca45018b96671
atxamov99 <atxamov99@inbox.ru>
2024-11-12 17:55:25 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+// let ism = prompt(`ismingizni kiriting`);
+// alert(ism);
+
+let massage = prompt(`ismingizni kiriting`);
+
+let choise = prompt(`bironta harf kiriting`)
+
+if (massage.includes(choise)) {
+  alert(`${massage} ismining ichida ${choise} harfi mavjud`);
+} else {
+  alert(`${massage} ismining ichida ${choise} harfi mavjud emas`);
+}
+
+// console.log(user.length)
+// alert(user.length)
+
+// console.log(user[3  ])
+// alert(user[3])
+
+// console.log(user.toUpperCase());
+// let admin = qora
+// console.log(user.toLowerCase())
+
+// console.log(user.indexOf(o))
+// alert(user.indexOf(o))
+
+// console.log(user.replace(va, yashil));
